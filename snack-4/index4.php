76b15945dd14f62a89eaf15775c986918542c69d
Edomak <edoardo.maccherini@gmail.com>
2021-06-29 12:51:06 +0200

    snack-4

---
+++ snack-4/index4.php
@@ -0,0 +1,70 @@
+<!-- Snack #4
+
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data.
+Stampare ogni data con i relativi post. -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snack4</title>
+    </head>
+    <body>
+
+    <?php
+
+        $posts = [
+            '10/01/2019' => [
+                [
+                    'title' => 'Post 1',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 1'
+                ],
+                [
+                    'title' => 'Post 2',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 2'
+                ],
+            ],
+            '10/02/2019' => [
+                [
+                    'title' => 'Post 3',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 3'
+                ]
+            ],
+            '15/05/2019' => [
+                [
+                    'title' => 'Post 4',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 4'
+                ],
+                [
+                    'title' => 'Post 5',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 5'
+                ],
+                [
+                    'title' => 'Post 6',
+                    'author' => 'Michele Papagni',
+                    'text' => 'Testo post 6'
+                ]
+            ],
+        ];
+
+        ?>
+        
+        <ul>
+            <?php foreach ($posts as $date => $post) { ?>
+                <?php foreach ($post as $item) { ?>
+                    <h3>Data e post:</h3>
+                    <li>
+                        <?php echo $date ?> : <?php echo $item["title"] ?> => <?php echo $item["text"] ?>
+                    </li>
+                <?php } ?>   
+            <?php } ?>
+        </ul>
+    </body>
+</html>
